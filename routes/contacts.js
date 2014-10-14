f24a7deae06d2ef7603825146788b5b453110542
Sten Hägglund <sten.hagglund@gmail.com>
2014-10-14 11:34:36 +0300

    Cleanup

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,61 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash-node');
+
+var respondResult = function(status, err, result){
+    if(err) { throw err; }
+    console.log(result);
+    res.json(result).status(status);
+  },
+  respondResultOk = _.partial(respondResult, 200)
+  respondResultCreated = _.partial(respondResult, 201);
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  var db = req.db;
-  var json_result = "{}";
-  db.collection('contactcollection').find().toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-	json_result = result;
-	res.json(json_result);
-  });
+  req.db.collection('contactcollection').find().toArray(respondResultOk);
 });
 
 router.get('/search', function(req, res) {
-  var db = req.db;
-  var json_result = "{}";
+  console.log("Search: " + req.query.q);
   var q = new RegExp(".*" + req.query.q + ".*", 'i');
-  db.collection('contactcollection').find({name: q}).toArray(function(err, result) {
-    console.log(result);
-	json_result = result;
-	res.json(json_result);
-  });
+  req.db.collection('contactcollection').find({name: q}).toArray(respondResultOk);
 });
 
 
 router.get('/:id', function(req, res) {
-  var db = req.db;
-  var json_result = "{}";
-  console.log("name: " + req.params.id);
-  db.collection('contactcollection').findById(req.params.id, function(err, result) {
-    console.log(result);
-	json_result = result;
-	res.json(json_result);
-  });
+  console.log("Fetch: " + req.params.id);
+  req.db.collection('contactcollection').findById(req.params.id, respondResultOk);
 });
 
 router.post('/', function(req, res) {
-  var db = req.db;
-  var name = req.body.name;
-  var phone = req.body.phone;
-  var email = req.body.email;
-  db.collection('contactcollection').insert({name: name, email: email, phone: phone}, function(err, result) {
-    if (err) throw err;
-    if (result) console.log('Added!');
-	res.json(result)
-  });
+  var data = _.pick(req.body, 'name', 'phone', 'email');
+  req.db.collection('contactcollection').insert(data, respondResultCreated);
 });
 
 router.delete('/:id', function(req, res) {
-  var db = req.db;
-  var json_result = "{}";
-  console.log("name: " + req.params.id);
-  db.collection('contactcollection').removeById(req.params.id, function(err, result) {
-    console.log(result);
-	res.send('Entry Deleted');
-  });
+  console.log("Delete: " + req.params.id);
+  req.db.collection('contactcollection').removeById(req.params.id, respondResultOk);
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 	res.json(json_result);
   });
 });
+
 router.get('/:name', function(req, res) {
   var db = req.db;
   var json_result = "{}";
